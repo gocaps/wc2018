e11b40a4b2a01f12f3a5aca50196dbcba4d0308b
Dave Kennedy <david.kennedy@ubc.ca>
2018-04-19 22:20:50 -0700

    initial commit

---
+++ wc2018_draw.php
@@ -0,0 +1,96 @@
+<?php
+
+############################
+# GROUP A
+############################
+
+$at1 = ['name' => 'Egypt', 'flag' => 'https://ssl.gstatic.com/onebox/media/sports/logos/lYah1Uqw37XdicC6C4HNqg_48x48.png', 'conf' => 'CAF'];
+$at2 = ['name' => 'Russia', 'flag' => 'https://ssl.gstatic.com/onebox/media/sports/logos/5Y6kOqiOIv2C1sP9C_BWtA_48x48.png', 'conf' => 'UEFA'];
+$at3 = ['name' => 'Saudi Arabia', 'flag' => 'https://ssl.gstatic.com/onebox/media/sports/logos/QoAJxO46fHid3_T-7nRZ0Q_48x48.png', 'conf' => 'AFC'];
+$at4 = ['name' => 'Uruguay', 'flag' => 'https://ssl.gstatic.com/onebox/media/sports/logos/KnSUdQWiGRoy89q4x85IgA_48x48.png', 'conf' => 'CONMEBOL'];
+$gA = _hlp_jsonify($at1, $at2, $at3, $at4);
+
+############################
+# GROUP B
+############################
+
+$bt1 = ['name' => 'Iran', 'flag' => 'https://ssl.gstatic.com/onebox/media/sports/logos/1oq8Fy7ETpBpZNaCA22ArQ_48x48.png', 'conf' => 'AFC'];
+$bt2 = ['name' => 'Morocco', 'flag' => 'https://ssl.gstatic.com/onebox/media/sports/logos/I3gt2Ew39ux3GGdZ-4JE3g_48x48.png', 'conf' => 'CAF'];
+$bt3 = ['name' => 'Portugal', 'flag' => 'https://ssl.gstatic.com/onebox/media/sports/logos/HJ3_2c4w791nZJj7n-Lj3Q_48x48.png', 'conf' => 'UEFA'];
+$bt4 = ['name' => 'Spain', 'flag' => 'https://ssl.gstatic.com/onebox/media/sports/logos/5hLkf7KFHhmpaiOJQv8LmA_48x48.png', 'conf' => 'UEFA'];
+$gB = _hlp_jsonify($bt1, $bt2, $bt3, $bt4);
+
+############################
+# GROUP C
+############################
+
+$ct1 = ['name' => 'Australia', 'flag' => 'https://ssl.gstatic.com/onebox/media/sports/logos/jSgw5z0EPOLzdUi-Aomq7Q_48x48.png', 'conf' => 'AFC'];
+$ct2 = ['name' => 'Denmark', 'flag' => 'https://ssl.gstatic.com/onebox/media/sports/logos/LaOvu-pyRqRso6uzff55XA_48x48.png', 'conf' => 'UEFA'];
+$ct3 = ['name' => 'France', 'flag' => 'https://ssl.gstatic.com/onebox/media/sports/logos/z3JEQB3coEAGLCJBEUzQ2A_48x48.png', 'conf' => 'UEFA'];
+$ct4 = ['name' => 'Peru', 'flag' => 'https://ssl.gstatic.com/onebox/media/sports/logos/U08wYdQptUaWuweG82L3dw_48x48.png', 'conf' => 'CONMEBOL'];
+$gC = _hlp_jsonify($ct1, $ct2, $ct3, $ct4);
+
+############################
+# GROUP D
+############################
+
+$dt1 = ['name' => 'Argentina', 'flag' => 'https://ssl.gstatic.com/onebox/media/sports/logos/1xBWyjjkA6vEWopPK3lIPA_48x48.png', 'conf' => 'CONMEBOL'];
+$dt2 = ['name' => 'Croatia', 'flag' => 'https://ssl.gstatic.com/onebox/media/sports/logos/9toerdOg8xW4CRhDaZxsyw_48x48.png', 'conf' => 'UEFA'];
+$dt3 = ['name' => 'Iceland', 'flag' => 'https://ssl.gstatic.com/onebox/media/sports/logos/QSlAlD9v6Fm_drC_2z1u8A_48x48.png', 'conf' => 'UEFA'];
+$dt4 = ['name' => 'Nigeria', 'flag' => 'https://ssl.gstatic.com/onebox/media/sports/logos/2vpR8clrEZdpNjaiHKpg7A_48x48.png', 'conf' => 'CAF'];
+$gD = _hlp_jsonify($dt1, $dt2, $dt3, $dt4);
+
+############################
+# GROUP E
+############################
+
+$et1 = ['name' => 'Brazil', 'flag' => 'https://ssl.gstatic.com/onebox/media/sports/logos/zKLzoJVYz0bb6oAnPUdwWQ_48x48.png', 'conf' => 'CONMEBOL'];
+$et2 = ['name' => 'Costa Rica', 'flag' => 'https://ssl.gstatic.com/onebox/media/sports/logos/ixZiM5pj2IvvYc15k-zfeQ_48x48.png', 'conf' => 'CONCACAF'];
+$et3 = ['name' => 'Switzerland', 'flag' => 'https://ssl.gstatic.com/onebox/media/sports/logos/1hy9ek4dOIffYULM6k1fqg_48x48.png', 'conf' => 'UEFA'];
+$et4 = ['name' => 'Serbia', 'flag' => 'https://ssl.gstatic.com/onebox/media/sports/logos/xyh1vmZ-xJH2iJCKjqS1Ow_48x48.png', 'conf' => 'UEFA'];
+$gE = _hlp_jsonify($et1, $et2, $et3, $et4);
+
+############################
+# GROUP F
+############################
+
+$ft1 = ['name' => 'Germany', 'flag' => 'https://ssl.gstatic.com/onebox/media/sports/logos/h1FhPLmDg9AHXzhygqvVPg_48x48.png', 'conf' => 'UEFA'];
+$ft2 = ['name' => 'South Korea', 'flag' => 'https://ssl.gstatic.com/onebox/media/sports/logos/Uu5pwNmMHGd5bCooKrS3Lw_48x48.png', 'conf' => 'AFC'];
+$ft3 = ['name' => 'Mexico', 'flag' => 'https://ssl.gstatic.com/onebox/media/sports/logos/yJF9xqmUGenD8108FJbg9A_48x48.png', 'conf' => 'CONCACAF'];
+$ft4 = ['name' => 'Sweden', 'flag' => 'https://ssl.gstatic.com/onebox/media/sports/logos/OkFlRvRsKMWb8Hk20L9Trw_48x48.png', 'conf' => 'UEFA'];
+$gF = _hlp_jsonify($ft1, $ft2, $ft3, $ft4);
+
+############################
+# GROUP G
+############################
+
+$gt1 = ['name' => 'Belgium', 'flag' => 'https://ssl.gstatic.com/onebox/media/sports/logos/6SF7yEoB60bU5knw-M7R5Q_48x48.png', 'conf' => 'UEFA'];
+$gt2 = ['name' => 'England', 'flag' => 'https://ssl.gstatic.com/onebox/media/sports/logos/DTqIL8Ba3KIuxGkpXw5ayA_48x48.png', 'conf' => 'UEFA'];
+$gt3 = ['name' => 'Panama', 'flag' => 'https://ssl.gstatic.com/onebox/media/sports/logos/JIn8OwxL6KFFiYrKGnL2RQ_48x48.png', 'conf' => 'CONCACAF'];
+$gt4 = ['name' => 'Tunisia', 'flag' => 'https://ssl.gstatic.com/onebox/media/sports/logos/Xs33c9XVUJBX0IkeFn_bIw_48x48.png', 'conf' => 'CAF'];
+$gG = _hlp_jsonify($gt1, $gt2, $gt3, $gt4);
+
+############################
+# GROUP H
+############################
+
+$ht1 = ['name' => 'Colombia', 'flag' => 'https://ssl.gstatic.com/onebox/media/sports/logos/tXHnA_tDylayacdjWQCJvw_48x48.png', 'conf' => 'CONMEBOL'];
+$ht2 = ['name' => 'Japan', 'flag' => 'https://ssl.gstatic.com/onebox/media/sports/logos/by4OltvtZz7taxuQtkiP3A_48x48.png', 'conf' => 'AFC'];
+$ht3 = ['name' => 'Poland', 'flag' => 'https://ssl.gstatic.com/onebox/media/sports/logos/yTS_Piy3M1wUBnqU0n5aAw_48x48.png', 'conf' => 'UEFA'];
+$ht4 = ['name' => 'Senegal', 'flag' => 'https://ssl.gstatic.com/onebox/media/sports/logos/zw3ac5sIbH4DS6zP5auOkQ_48x48.png', 'conf' => 'CAF'];
+$gH = _hlp_jsonify($ht1, $ht2, $ht3, $ht4);
+
+############################
+# DRAW
+############################
+
+$draw = ['groupA' => $gA, 'groupB' => $gB, 'groupC' => $gC, 'groupD' => $gD, 'groupE' => $gE, 'groupF' => $gF, 'groupG' => $gG, 'groupH' => $gH];
+echo json_encode($draw);
+
+function _hlp_jsonify($type, &$t1, &$t2, &$t3, &$t4) {
+	$g = [];
+	for($i=1; $i<=4; $i++) {
+		$g["team$i"] = ${"t".$i};
+	}
+	return $g;
+}
